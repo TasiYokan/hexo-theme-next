10ef33a6f304885fff4a4f016372559b74a02fe9
TasiYokan <guanghao.yang@outlook.com>
2017-07-04 21:06:11 +0800

    Support hide desc in post page

--- scripts/image-mgr.js
+++ scripts/image-mgr.js
@@ -3,8 +3,8 @@ hexo.extend.tag.register('qiniu', function(args){
     if (!slug) return;
 
     var src = "http://osg7voxw6.bkt.clouddn.com/" + slug;
-    var alt = args.length ? args.shift() : '';
     var title = args.length ? args.shift() : '';
+    var alt = args.length ? args.shift() : '';
 
     return ' <img src="' + src + '" alt="' + alt + '" title="' + title + '">';
 });
