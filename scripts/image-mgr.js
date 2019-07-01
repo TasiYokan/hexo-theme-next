cc2de6e517e0c63f2bee988693092c41837c01c5
TasiYokan <guanghao.yang@outlook.com>
2019-07-01 11:15:28 +0800

    Add new img host source

--- scripts/image-mgr.js
+++ scripts/image-mgr.js
@@ -8,3 +8,14 @@ hexo.extend.tag.register('qiniu', function(args){
 
     return ' <img src="' + src + '" alt="' + alt + '" title="' + title + '">';
 });
+
+hexo.extend.tag.register('tencent', function (args) {
+    var slug = args.shift();
+    if (!slug) return;
+
+    var src = "https://imgs-1259535704.cos.ap-guangzhou.myqcloud.com/blog/" + slug;
+    var title = args.length ? args.shift() : '';
+    var alt = args.length ? args.shift() : '';
+
+    return ' <img src="' + src + '" alt="' + alt + '" title="' + title + '">';
+});
