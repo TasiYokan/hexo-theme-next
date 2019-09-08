967d0350818ffa152ad5b06f6b3f229125208276
TasiYokan <guanghao.yang@outlook.com>
2019-09-08 19:30:12 +0800

    Use XMLHttpRequest to test connection

--- source/js/src/blocked-video.js
+++ source/js/src/blocked-video.js
@@ -1,19 +1,38 @@
-var vinmeoIcon = new Image();
-vinmeoIcon.onerror = function () {
+var ReplaceWithImg = function() {
     // The user can't access video from blocked source like vimeo youtube
     console.log("can't access vimeo")
 
-    var videoFrame = document.getElementById("iframe-blocked");
-    // var width = videoFrame.getAttribute("width");
-    var altImgUrl = videoFrame.getAttribute("altImgUrl");
-    var altImgTxt = videoFrame.getAttribute("alt");
+    var videoFrames = document.getElementsByName("iframe-blocked");
+    console.log("length "+videoFrames.length);
+    for (var i = videoFrames.length - 1; i >= 0; i--) {
+        console.log("i "+i);
+        var videoFrame = videoFrames[i];
 
-    var altImg = document.createElement("IMG");
-    // altImg.setAttribute("width", width);
-    altImg.setAttribute("src", altImgUrl);
-    altImg.setAttribute("alt", altImgTxt)
+        console.log("url " + videoFrame.getAttribute("src"));
+        // var width = videoFrame.getAttribute("width");
+        var altImgUrl = videoFrame.getAttribute("altImgUrl");
+        var altImgTxt = videoFrame.getAttribute("alt");
 
-    videoFrame.parentNode.replaceChild(altImg, videoFrame);
-    altImg.parentNode.setAttribute("style", "position: relative; margin-bottom: 20px; width: 100%; padding-top: 0%;");
+        var altImg = document.createElement("IMG");
+        // altImg.setAttribute("width", width);
+        altImg.setAttribute("src", altImgUrl);
+        altImg.setAttribute("alt", altImgTxt)
+
+        videoFrame.parentNode.replaceChild(altImg, videoFrame);
+        altImg.parentNode.setAttribute("style", "position: relative; margin-bottom: 20px; width: 100%; padding-top: 0%;");
+    }
+}
+
+var xhr = new XMLHttpRequest();
+xhr.timeout = 3000;
+xhr.responseType = "text";
+xhr.open('GET', 'https://vimeo.com/', true);
+xhr.ontimeout = function (e) { 
+    console.log("vimeo timeout!")
+    ReplaceWithImg();
 };
-vinmeoIcon.src = "http://vimeo.com/favicon.ico";
+xhr.onerror = function (e) {
+    console.log("vimeo error!")
+    ReplaceWithImg();
+}
+xhr.send();
